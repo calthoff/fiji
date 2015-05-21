577a269ae3a62d7fd91f362ceb05cd573c993ff9
Cory <coryedwardalthoff@gmail.com>
2015-05-21 01:12:35 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function chunk(arr, size) {
+    var newArr = [];
+    for (var i=0; i<arr.length; i+=size) {
+        newArr.push(arr.slice(i, i+size));
+    }
+    return newArr;
+}
+
+(function(){
+    var app = angular.module('symphony', []);
+    app.controller('scController', ['$http', function($http){
+        store = this;
+        store.products = [];
+        store.final_product = [];
+        $http.get('http://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js').success
+        (function (data) {
+            for (i in data.products) {
+                store.products.push([data.products[i].mainImage.ref,
+                    data.products[i].defaultPriceInCents/100, data.products[i].name]);
+            }
+            console.log(store.products);
+            store.final_product = chunk(store.products,3);
+            console.log(store.final_product);
+        });
+    }]);
+})();
